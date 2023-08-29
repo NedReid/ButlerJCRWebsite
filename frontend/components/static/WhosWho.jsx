f83dc7034168b86cdb25ae872fe88770bf6854e1
Ned Reid <bzrr62@durham.ac.uk>
2023-08-29 15:58:59 +0100

    42: Change Who's Who section

--- frontend/components/static/WhosWho.jsx
+++ frontend/components/static/WhosWho.jsx
@@ -12,20 +12,20 @@ class WhosWho extends React.Component {
             <div className="my-4 text-5xl font-semibold">Who's Who?</div>
             <div className="my-4 text-3xl font-semibold">JCR Exec</div>
             <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3">
-                <WhosWhoBlock name="Urwah Mirza" pronouns="She/Her" role="JCR President" photo="urwah.jpg" email="butler.jcr@durham.ac.uk"/>
-                <WhosWhoBlock name="Joseph Chesters" pronouns="He/Him" role="FACSO" photo="joe.jpg" email="butler.facso@durham.ac.uk"/>
-                <WhosWhoBlock name="Ned Reid" pronouns="He/Him" role="JCR Chair" photo="ned.jpg" email="butler.chair@durham.ac.uk"/>
-                <WhosWhoBlock name="Morgan Miller" pronouns="She/Her" role="Societies Officer" photo="morgan.jpg" email="butler.societies@durham.ac.uk"/>
-                <WhosWhoBlock name="Georgina Solly" pronouns="She/They" role="Sports Officer" photo="georgina.jpg" email="rnvp64@durham.ac.uk"/>
-                <WhosWhoBlock name="Chloé Margaux" pronouns="She/Her" role="Social Chair" photo="chloe.jpg" email="rngm25@durham.ac.uk"/>
-                <WhosWhoBlock name="Daniel Stuart" pronouns="He/Him" role="Vice-President" photo="dan.jpg" email="jb.vicepresident@durham.ac.uk"/>
-                <WhosWhoBlock name="Emily Wilson" pronouns="She/Her" role="JBs Officer" photo="emily.jpg" email="pnhm47@durham.ac.uk"/>
-                <WhosWhoBlock name="Harry Clipston" pronouns="He/Him" role="Technical Director" photo="harry.jpg" email="jbtechcomm@gmail.com"/>
-                <WhosWhoBlock name="Mariah Bennett" pronouns="She/Her" role="Welfare Officer" photo="mariah.jpg" email="butler.welfarejcr@durham.ac.uk"/>
-                <WhosWhoBlock name="Molly Knox" pronouns="She/They" role="Welfare Officer" photo="molly.jpg" email="butler.welfarejcr@durham.ac.uk"/>
-                <WhosWhoBlock name="Quentin O'Brien" pronouns="He/Him" role="Treasurer" photo="quentin.jpg" email="butler.treasurer@durham.ac.uk"/>
-                <WhosWhoBlock name="Rosaleen Tite-Ahern" pronouns="She/Her" role="Publicity Officer" photo="rosaleen.jpg" email="xsrr55@durham.ac.uk"/>
-                <WhosWhoBlock name="Saujanya Vinayy" pronouns="She/Her" role="International Officer" photo="saujanya.jpg" email="tbfh39@durham.ac.uk"/>
+                <WhosWhoBlock name="Daniel Stuart " pronouns="He/Him" role="JCR President" photo="2023/dans.webp" email="butler.jcr@durham.ac.uk"/>
+                <WhosWhoBlock name="Arman Huq" pronouns="He/Him" role="FACSO" photo="2023/armanh.webp" email="butler.facso@durham.ac.uk"/>
+                <WhosWhoBlock name="Eloise Fleming" pronouns="She/Her" role="JCR Chair" photo="2023/eloisef.webp" email="butler.chair@durham.ac.uk"/>
+                <WhosWhoBlock name="Lottie Gibson" pronouns="She/Her" role="Societies Officer" photo="2023/lottieg.webp" email="butler.societies@durham.ac.uk"/>
+                <WhosWhoBlock name="Niamh Morton" pronouns="She/Her" role="Sports Officer" photo="jb.jpg" email="niamh.e.morton@durham.ac.uk"/>
+                <WhosWhoBlock name="Alice Lee" pronouns="She/Her" role="Social Chair" photo="2023/alicel.webp" email="alice.lee@durham.ac.uk"/>
+                <WhosWhoBlock name="Ben Baker" pronouns="He/Him" role="Vice-President" photo="2023/benb.webp" email="ben.baker@durham.ac.uk"/>
+                <WhosWhoBlock name="Shannon Southern" pronouns="She/Her" role="JBs Officer" photo="2023/shannons.webp" email="shannon.e.southern@durham.ac.uk"/>
+                {/*<WhosWhoBlock name="Harry Clipston" pronouns="He/Him" role="Technical Director" photo="2023/dans.webp" email="jbtechcomm@gmail.com"/>*/}
+                <WhosWhoBlock name="Rosa Aziz" pronouns="They/She" role="Welfare Officer" photo="2023/rosaa.webp" email="butler.welfarejcr@durham.ac.uk"/>
+                <WhosWhoBlock name="Mathilde Wilson-Drummond" pronouns="She/Her" role="Welfare Officer" photo="2023/mathildewd.webp" email="butler.welfarejcr@durham.ac.uk"/>
+                <WhosWhoBlock name="Jakub Kostrzewa" pronouns="He/Him" role="Treasurer" photo="2023/jakubk.webp" email="butler.treasurer@durham.ac.uk"/>
+                <WhosWhoBlock name="Hazel Cheung" pronouns="She/Her" role="Publicity Officer" photo="2023/hazelc.webp" email="hazel.m.cheung@durham.ac.uk"/>
+                <WhosWhoBlock name="Zeni (Xeni) Pun" pronouns="She/Her" role="International Officer" photo="2023/xenip.webp" email="zeni.pun@durham.ac.uk"/>
             </div>
         </div>
 
